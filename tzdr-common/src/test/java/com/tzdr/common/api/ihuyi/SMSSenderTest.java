c403d8b5925981bcc27176750543182a4f6ec289
invtool_hubo <hubo@invtool.com>
2016-07-11 19:32:50 +0800

    1、短信配置文件调整；2、测试、生产配置文件调整。

--- tzdr-common/src/test/java/com/tzdr/common/api/ihuyi/SMSSenderTest.java
+++ tzdr-common/src/test/java/com/tzdr/common/api/ihuyi/SMSSenderTest.java
@@ -3,16 +3,19 @@ package com.tzdr.common.api.ihuyi;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Test;
+
 public class SMSSenderTest {
 
-	//@Test
+	@Test
 	//@Ignore
 	public void test() {
-		int smsChannel = 1;
-		Map<String,String> map= new HashMap<String,String>();
-		map.put("module", "test");
-		map.put("code", "test2");
-		SMSSender.getInstance().sendByTemplate(smsChannel, "18996152105", "8800.template", null);
+//		int smsChannel = 1;
+//		Map<String,String> map= new HashMap<String,String>();
+//		map.put("module", "test");
+//		map.put("code", "test2");
+//		SMSSender.getInstance().sendByTemplate(smsChannel, "13896679979", "8800.template", null);
+		SMSSender.getInstance().sendSMS("13896679979", "您的验证码是：【变量】。请不要把验证码泄露给其他人。");
 	}
 
 }
