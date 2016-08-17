bf4173aaf56a97f62986a3ca409376943c6d4801
username <username@WINDOWS-OJPLVSN>
2016-08-17 15:30:07 +0800

    1.手机号码验证bug修改，

--- tzdr-app/src/main/java/com/tzdr/api/util/RequestUtils.java
+++ tzdr-app/src/main/java/com/tzdr/api/util/RequestUtils.java
@@ -172,11 +172,11 @@ public class RequestUtils{
 	 * @param mobileNum
 	 * @return
 	 */
-	public static boolean isMobileNum(String mobileNum) {
+	public static boolean isMobileNum(String mobileNum) { 
 		if (null == mobileNum) {
 			return false;
 		}
-		return mobileNum.matches("^((13[0-9])|(14[0-9])|(15[0-9])|(17[0-9])|(18[0-9]))(\\d{8})$");
+		return mobileNum.matches("^(((13[0-9])|(14[7])|(15[0-9])|(17[0-9])|(18[0-9]))+\\d{8})$");
 	}
 	
 	/**
