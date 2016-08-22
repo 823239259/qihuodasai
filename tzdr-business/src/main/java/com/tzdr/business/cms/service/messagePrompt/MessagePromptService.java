7a82ae9be74eef5dc299c9b63746be51d71014f3
vs <vs@LAPTOP-CUUOJ0TU>
2016-08-22 21:00:36 +0800

    修改发送邮件

--- tzdr-business/src/main/java/com/tzdr/business/cms/service/messagePrompt/MessagePromptService.java
+++ tzdr-business/src/main/java/com/tzdr/business/cms/service/messagePrompt/MessagePromptService.java
@@ -32,42 +32,42 @@ public class MessagePromptService {
 				if (submitType.equalsIgnoreCase(PromptTypes.isFutures)) {
 					fristParam = "申请列表";
 					twoParam = "期货方案";
-					url = "http://test.manage.vs.com/admin/internation/future/list";
+					url = "http://manage.vs.com/admin/internation/future/list";
 					typeKey = "riskEmail";
 				} else if (submitType.equalsIgnoreCase(PromptTypes.isAddBond)) {
 					fristParam = "补充保证金";
 					twoParam = "补充保证金";
-					url = "http://test.manage.vs.com/admin/internation/future/list";
+					url = "http://manage.vs.com/admin/internation/future/list";
 					typeKey = "riskEmail";
 				} else if (submitType.equalsIgnoreCase(PromptTypes.isEndScheme)) {
 					fristParam = "方案管理";
 					twoParam = "申请结算方案的";
-					url = "http://test.manage.vs.com/admin/internation/future/list";
+					url = "http://manage.vs.com/admin/internation/future/list";
 					typeKey = "riskEmail";
 				} else if (submitType.equalsIgnoreCase(PromptTypes.isAlipayRecharge)) {
 					fristParam = "支付宝充值审核";
 					twoParam = "支付宝充值";
-					url = "http://test.manage.vs.com/admin/rechargeReview/list";
+					url = "http://manage.vs.com/admin/rechargeReview/list";
 					typeKey = "fundEmail";
 				} else if (submitType.equalsIgnoreCase(PromptTypes.isBankReCharge)) {
 					fristParam = "银行转账充值审核";
 					twoParam = "银行转账充值";
-					url = "http://test.manage.vs.com/admin/rechargeReview/list";
+					url = "http://manage.vs.com/admin/rechargeReview/list";
 					typeKey = "fundEmail";
 				} else if (submitType.equalsIgnoreCase(PromptTypes.isLineTransfer)) {
 					fristParam = "线下转账待审核";
 					twoParam = "线下转账提现";
-					url = "http://test.manage.vs.com/admin/withdrawAudit/list";
+					url = "http://manage.vs.com/admin/withdrawAudit/list";
 					typeKey = "fundEmail";
 				} else if (submitType.equalsIgnoreCase(PromptTypes.isTheTrial)) {
 					fristParam = "提现待审核【初审】";
 					twoParam = "提现";
-					url = "http://test.manage.vs.com/admin/withdrawAudit/list";
+					url = "http://manage.vs.com/admin/withdrawAudit/list";
 					typeKey = "fundEmail";
 				} else if (submitType.equalsIgnoreCase(PromptTypes.isReview)) {
 					fristParam = "提现待审核【复审】";
 					twoParam = "提现";
-					url = "http://test.manage.vs.com/admin/withdrawAudit/list";
+					url = "http://manage.vs.com/admin/withdrawAudit/list";
 					typeKey = "reviewEmail";
 				}
 				if (typeKey != null && typeKey.length() > 0) {
