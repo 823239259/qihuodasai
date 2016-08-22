ca7c0226d68af825243244f67622d798154b05e5
vs <vs@LAPTOP-CUUOJ0TU>
2016-08-22 19:35:51 +0800

    优化发送邮件

--- tzdr-business/src/main/java/com/tzdr/business/cms/service/messagePrompt/MessagePromptService.java
+++ tzdr-business/src/main/java/com/tzdr/business/cms/service/messagePrompt/MessagePromptService.java
@@ -21,74 +21,84 @@ public class MessagePromptService {
 	@Autowired
 	private DataMapService dataMapService;
 
-	public void sendMessage(String submitType, String userName) {
+	public void sendMessage(final String submitType,final String userName) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				String fristParam = "";
+				String twoParam = "";
+				String url = "";
+				String typeKey = "";
+				if (submitType.equalsIgnoreCase(PromptTypes.isFutures)) {
+					fristParam = "申请列表";
+					twoParam = "期货方案";
+					url = "http://test.manage.vs.com/admin/internation/future/list";
+					typeKey = "riskEmail";
+				} else if (submitType.equalsIgnoreCase(PromptTypes.isAddBond)) {
+					fristParam = "补充保证金";
+					twoParam = "补充保证金";
+					url = "http://test.manage.vs.com/admin/internation/future/list";
+					typeKey = "riskEmail";
+				} else if (submitType.equalsIgnoreCase(PromptTypes.isEndScheme)) {
+					fristParam = "方案管理";
+					twoParam = "申请结算方案的";
+					url = "http://test.manage.vs.com/admin/internation/future/list";
+					typeKey = "riskEmail";
+				} else if (submitType.equalsIgnoreCase(PromptTypes.isAlipayRecharge)) {
+					fristParam = "支付宝充值审核";
+					twoParam = "支付宝充值";
+					url = "http://test.manage.vs.com/admin/rechargeReview/list";
+					typeKey = "fundEmail";
+				} else if (submitType.equalsIgnoreCase(PromptTypes.isBankReCharge)) {
+					fristParam = "银行转账充值审核";
+					twoParam = "银行转账充值";
+					url = "http://test.manage.vs.com/admin/rechargeReview/list";
+					typeKey = "fundEmail";
+				} else if (submitType.equalsIgnoreCase(PromptTypes.isLineTransfer)) {
+					fristParam = "线下转账待审核";
+					twoParam = "线下转账提现";
+					url = "http://test.manage.vs.com/admin/withdrawAudit/list";
+					typeKey = "fundEmail";
+				} else if (submitType.equalsIgnoreCase(PromptTypes.isTheTrial)) {
+					fristParam = "提现待审核【初审】";
+					twoParam = "提现";
+					url = "http://test.manage.vs.com/admin/withdrawAudit/list";
+					typeKey = "fundEmail";
+				} else if (submitType.equalsIgnoreCase(PromptTypes.isReview)) {
+					fristParam = "提现待审核【复审】";
+					twoParam = "提现";
+					url = "http://test.manage.vs.com/admin/withdrawAudit/list";
+					typeKey = "reviewEmail";
+				}
+				if (typeKey != null && typeKey.length() > 0) {
+					List<DataMap> dataMapList = dataMapService.findByTypeKey(typeKey);
+					for (DataMap dataMap2 : dataMapList) {
+						List<String> list = new ArrayList<>();
+						list.add(fristParam);
+						list.add(dataMap2.getTypeName());
+						list.add(userName);
+						list.add(twoParam);
+						list.add(url);
+						EmailUtils emailUtils = EmailUtils.getInstance();
+						try {
+							boolean b=emailUtils.sendBatchMailTemp("审核", null, "messagePrompt", ".ftl", list, null,
+									dataMap2.getValueName());
+							if(b){
+								logger.info("发送给"+dataMap2.getTypeName()+"的邮件成功！！");
+							}else{
+								logger.info("发送给"+dataMap2.getTypeName()+"的邮件失败！！");
+							}
+							
+						} catch (Exception e) {
+							logger.info("邮件发送异常",e);
 
-		String fristParam = "";
-		String twoParam = "";
-		String url = "";
-		String typeKey = "";
-		if (submitType.equalsIgnoreCase(PromptTypes.isFutures)) {
-			fristParam = "申请列表";
-			twoParam = "期货方案";
-			url = "http://test.manage.vs.com/admin/internation/future/list";
-			typeKey = "riskEmail";
-		} else if (submitType.equalsIgnoreCase(PromptTypes.isAddBond)) {
-			fristParam = "补充保证金";
-			twoParam = "补充保证金";
-			url = "http://test.manage.vs.com/admin/internation/future/list";
-			typeKey = "riskEmail";
-		} else if (submitType.equalsIgnoreCase(PromptTypes.isEndScheme)) {
-			fristParam = "方案管理";
-			twoParam = "申请结算方案的";
-			url = "http://test.manage.vs.com/admin/internation/future/list";
-			typeKey = "riskEmail";
-		} else if (submitType.equalsIgnoreCase(PromptTypes.isAlipayRecharge)) {
-			fristParam = "支付宝充值审核";
-			twoParam = "支付宝充值";
-			url = "http://test.manage.vs.com/admin/rechargeReview/list";
-			typeKey = "fundEmail";
-		} else if (submitType.equalsIgnoreCase(PromptTypes.isBankReCharge)) {
-			fristParam = "银行转账充值审核";
-			twoParam = "银行转账充值";
-			url = "http://test.manage.vs.com/admin/rechargeReview/list";
-			typeKey = "fundEmail";
-		} else if (submitType.equalsIgnoreCase(PromptTypes.isLineTransfer)) {
-			fristParam = "线下转账待审核";
-			twoParam = "线下转账提现";
-			url = "http://test.manage.vs.com/admin/withdrawAudit/list";
-			typeKey = "fundEmail";
-		} else if (submitType.equalsIgnoreCase(PromptTypes.isTheTrial)) {
-			fristParam = "提现待审核【初审】";
-			twoParam = "提现";
-			url = "http://test.manage.vs.com/admin/withdrawAudit/list";
-			typeKey = "fundEmail";
-		} else if (submitType.equalsIgnoreCase(PromptTypes.isReview)) {
-			fristParam = "提现待审核【复审】";
-			twoParam = "提现";
-			url = "http://test.manage.vs.com/admin/withdrawAudit/list";
-			typeKey = "reviewEmail";
-		}
-		if (typeKey != null && typeKey.length() > 0) {
-			List<DataMap> dataMapList = dataMapService.findByTypeKey(typeKey);
-			for (DataMap dataMap2 : dataMapList) {
-				List<String> list = new ArrayList<>();
-				list.add(fristParam);
-				list.add(dataMap2.getTypeName());
-				list.add(userName);
-				list.add(twoParam);
-				list.add(url);
-				EmailUtils emailUtils = EmailUtils.getInstance();
-				try {
-					emailUtils.sendBatchMailTemp("审核", null, "messagePrompt", ".ftl", list, null,
-							dataMap2.getValueName());
-					logger.info("发送给"+dataMap2.getTypeName()+"的邮件成功！！");
-				} catch (Exception e) {
-					e.printStackTrace();
+						}
+					}
 
 				}
 			}
-
-		}
+		}).start();
+		
 	}
 
 }
