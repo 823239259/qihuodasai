5a2b7a05d9a95e863dfd2b4df0d89c163f7ee3c2
vs <vs@LAPTOP-CUUOJ0TU>
2016-09-13 19:07:34 +0800

    2.增加所有用户列表的新增字段

--- tzdr-domain/src/main/java/com/tzdr/domain/vo/WuserListVo.java
+++ tzdr-domain/src/main/java/com/tzdr/domain/vo/WuserListVo.java
@@ -1,6 +1,7 @@
 package com.tzdr.domain.vo;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import com.tzdr.common.utils.SqlColumn;
@@ -18,47 +19,32 @@ import com.tzdr.domain.cache.CacheManager;
 public class WuserListVo implements Serializable {
 	
 	private static final long serialVersionUID = 5413198153714337951L;
-	
 	@SqlColumn
 	private String id;
-	
 	@SqlColumn
 	private String tname;
-	
 	@SqlColumn
 	private String uname;//用户昵称
-	
 	@SqlColumn
 	private String email;//邮箱
-	
 	@SqlColumn
 	private String mobile;//手机号
-	
 	@SqlColumn
 	private BigInteger ctime;//注册时间
-	
-	private String ctimeStr;
-	
 	/**
 	 * 用户类型
 	 */
 	@SqlColumn
 	private String userType;
-	
 	@SqlColumn
 	private String idcard;//身份证号
-	
 	@SqlColumn
 	private BigInteger lastLoginTime;//最后登陆时间
-	
-	private String lastLoginTimeStr;
-	
 	/**
 	 * 账户余额
 	 */
 	@SqlColumn
 	private Double avlBal;
-	
 	/**
 	 * 配资保证金
 	 */
@@ -67,40 +53,43 @@ public class WuserListVo implements Serializable {
 	//冻结金额
 	@SqlColumn
 	private Double frzBal;
-	
 	/**
 	 * 支付宝帐号
 	 */
 	@SqlColumn
 	private String alipayAccount;
-	
 	/**
 	 * 来源网站
 	 */
 	@SqlColumn
 	private Integer source;
-	private String sourceName;
 	@SqlColumn
 	private String channel;
 	@SqlColumn
 	private String keyword;
+	@SqlColumn
+	private Double totalCharge;//总充值金额
+	@SqlColumn
+	private BigDecimal totalOperate;//总申请操盘金额
+	@SqlColumn
+	private BigDecimal htranActualLever;//恒指操盘手数
+	@SqlColumn
+	private BigDecimal ytranActualLever;//原油操盘手数
+	@SqlColumn
+	private BigDecimal atranActualLever;//富时A50操盘手数
+	@SqlColumn
+	private BigDecimal interActualLever;//国际综合操盘手数
+	@SqlColumn
+	private Double withDrawMoney;//申请提现金额
+
+	private String ctimeStr;
 	
-	public String getChannel() {
-		return channel;
-	}
-
-	public void setChannel(String channel) {
-		this.channel = channel;
-	}
-
-	public String getKeyword() {
-		return keyword;
-	}
-
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
-	}
-
+	private String lastLoginTimeStr;
+	
+	private String sourceName;
+	
+	
+	
 	public String getId() {
 		return id;
 	}
@@ -197,6 +186,98 @@ public class WuserListVo implements Serializable {
 		this.frzBal = frzBal;
 	}
 
+	public String getAlipayAccount() {
+		return alipayAccount;
+	}
+
+	public void setAlipayAccount(String alipayAccount) {
+		this.alipayAccount = alipayAccount;
+	}
+
+	public Integer getSource() {
+		return source;
+	}
+
+	public void setSource(Integer source) {
+		this.source = source;
+	}
+
+	public String getChannel() {
+		return channel;
+	}
+
+	public void setChannel(String channel) {
+		this.channel = channel;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	
+
+	public Double getTotalCharge() {
+		return totalCharge;
+	}
+
+	public void setTotalCharge(Double totalCharge) {
+		this.totalCharge = totalCharge;
+	}
+
+	
+
+	public BigDecimal getTotalOperate() {
+		return totalOperate;
+	}
+
+	public void setTotalOperate(BigDecimal totalOperate) {
+		this.totalOperate = totalOperate;
+	}
+
+	public BigDecimal getHtranActualLever() {
+		return htranActualLever;
+	}
+
+	public void setHtranActualLever(BigDecimal htranActualLever) {
+		this.htranActualLever = htranActualLever;
+	}
+
+	public BigDecimal getYtranActualLever() {
+		return ytranActualLever;
+	}
+
+	public void setYtranActualLever(BigDecimal ytranActualLever) {
+		this.ytranActualLever = ytranActualLever;
+	}
+
+	public BigDecimal getAtranActualLever() {
+		return atranActualLever;
+	}
+
+	public void setAtranActualLever(BigDecimal atranActualLever) {
+		this.atranActualLever = atranActualLever;
+	}
+
+	public BigDecimal getInterActualLever() {
+		return interActualLever;
+	}
+
+	public void setInterActualLever(BigDecimal interActualLever) {
+		this.interActualLever = interActualLever;
+	}
+
+	public Double getWithDrawMoney() {
+		return withDrawMoney;
+	}
+
+	public void setWithDrawMoney(Double withDrawMoney) {
+		this.withDrawMoney = withDrawMoney;
+	}
+
 	public String getCtimeStr() {
 		if (this.ctime != null) {
 			this.ctimeStr = TypeConvert.long1000ToDatetimeStr(this.ctime.longValue());
@@ -219,22 +300,7 @@ public class WuserListVo implements Serializable {
 		this.lastLoginTimeStr = lastLoginTimeStr;
 	}
 
-	public String getAlipayAccount() {
-		return alipayAccount;
-	}
-
-	public void setAlipayAccount(String alipayAccount) {
-		this.alipayAccount = alipayAccount;
-	}
-
-	public Integer getSource() {
-		return source;
-	}
-
-	public void setSource(Integer source) {
-		this.source = source;
-	}
-
+	
 	public String getSourceName() {
 		if(this.source == 2) {
 			sourceName = "配股宝Wap";
@@ -262,5 +328,7 @@ public class WuserListVo implements Serializable {
 	public void setSourceName(String sourceName) {
 		this.sourceName = sourceName;
 	}
+
+
 
 }
