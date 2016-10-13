727da9649cd34fb48372fcc83c99dd72cc207947
gx <gx@DESKTOP-MHC2QL0>
2016-10-13 14:28:30 +0800

    1

--- tzdr-web/src/main/webapp/static/script/qutrade/quote.trade.js
+++ tzdr-web/src/main/webapp/static/script/qutrade/quote.trade.js
@@ -205,22 +205,22 @@ $(function() {
 /*渐变涨*/
 function rise(cls){
 	$("."+cls+"").addClass("rise");
-    $("."+cls+"").animate({
+   /* $("."+cls+"").animate({
         opacity: '0'
-    },500);
+    },500);*/
     setTimeout(function() {
-        $("."+cls+"").animate({ opacity: '1' },0);
+        //$("."+cls+"").animate({ opacity: '1' },0);
         $("."+cls+"").removeClass("rise");
-    },500);
+    },1000);
 }
 /*渐变跌*/
 function fall(cls){
 	$("."+cls+"").addClass("fall");
-    $("."+cls+"").animate({
+    /*$("."+cls+"").animate({
         opacity: '0'
-    },500);
+    },500);*/
     setTimeout(function() {
-        $("."+cls+"").animate({ opacity: '1' },0);
+        //$("."+cls+"").animate({ opacity: '1' },0);
         $("."+cls+"").removeClass("fall");
-    },500);
+    },1000);
 }
