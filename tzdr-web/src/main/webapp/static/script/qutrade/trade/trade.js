1cd1b5a8bcaf8fa975168dfc15869906237a466d
hedaoqing <1143769121@qq.com>
2016-11-22 15:49:55 +0800

    1.交易行情转换html页面

--- tzdr-web/src/main/webapp/static/script/qutrade/trade/trade.js
+++ tzdr-web/src/main/webapp/static/script/qutrade/trade/trade.js
@@ -86,6 +86,7 @@ function handleData(evt){
 				setIsLogin(true);
 				loginFail = false;
 				anotherPlace = false;
+				layer.msg('交易服务器连接成功', {icon: 4});
 			} else {
 				loginFail = -2;
 				tipAlert(loginMessage);
