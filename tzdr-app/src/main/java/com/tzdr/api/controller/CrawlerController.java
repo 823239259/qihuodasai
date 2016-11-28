7599c73c15d2443c1b03fc0c6dddda6e71b22d13
hedaoqing <1143769121@qq.com>
2016-11-28 14:29:40 +0800

    1.实时数据增加接口，按照类型查询，日历数据增加接口-按照时间段查询

--- tzdr-app/src/main/java/com/tzdr/api/controller/CrawlerController.java
+++ tzdr-app/src/main/java/com/tzdr/api/controller/CrawlerController.java
@@ -16,6 +16,7 @@ import com.tzdr.business.service.crawler.CrawlerCalendarService;
 import com.tzdr.business.service.crawler.CrawlerWallstreetnLiveContentService;
 import com.tzdr.business.service.crawler.CrawlerWallstreetnLiveService;
 import com.tzdr.common.utils.Page;
+import com.tzdr.common.web.support.JsonResult;
 import com.tzdr.domain.web.entity.CrawlerWallstreetnLive;
 
 @Controller
@@ -41,6 +42,18 @@ public class CrawlerController {
 		return result;
 	}
 	/**
+	 * 获取实时新闻数据
+	 * @param crawlerWallstreetnLive
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/getCrawlerByChannel",method = RequestMethod.GET)
+	public JsonResult getCrawlerByChannel(CrawlerWallstreetnLive crawlerWallstreetnLive,HttpServletRequest request,@RequestParam("channelset")String channelset){
+		JsonResult result = new JsonResult(true);
+		result.appendData("data", crawlerWallstreetnLiveService.getCrawler(new Page(request),channelset));
+		return result;
+	}
+	/**
 	 * 获取日历
 	 * @return
 	 */
@@ -51,6 +64,21 @@ public class CrawlerController {
 		result.setSuccess(true);
 		result.setData(crawlerCalendarService.doGetCrwlerCalendar(new Page(request)));
 		return result;
+	}
+	/**
+	 * 获取日历数据
+	 * @param request
+	 * @param startTime
+	 * @param endTime
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/getCrawlerCalendarByTime",method = RequestMethod.GET)
+	public JsonResult getCrawlerCalendarByTime(HttpServletRequest request,@RequestParam("startTime") String startTime,@RequestParam("endTime") String endTime){
+		JsonResult resultJson = new JsonResult();
+		resultJson.setSuccess(true);
+		resultJson.appendData("data",crawlerCalendarService.doGetCrwlerCalendarByTime(new Page(request), startTime, endTime));
+		return resultJson;
 	}
 	/**
 	 * 实时数据内容
