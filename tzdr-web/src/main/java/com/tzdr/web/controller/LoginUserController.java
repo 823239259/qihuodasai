596afd1c0834faca64c91b5b4a460e434184f297
hedaoqing <1143769121@qq.com>
2016-12-01 19:44:20 +0800

    1

--- tzdr-web/src/main/java/com/tzdr/web/controller/LoginUserController.java
+++ tzdr-web/src/main/java/com/tzdr/web/controller/LoginUserController.java
@@ -1,5 +1,7 @@
 package com.tzdr.web.controller;
 
+import java.io.UnsupportedEncodingException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.tzdr.business.service.wuser.WUserService;
 import com.tzdr.common.web.support.JsonResult;
 import com.tzdr.domain.web.entity.WUser;
+import com.tzdr.web.constants.Constants;
+import com.tzdr.web.utils.UserSessionBean;
 
 import jodd.util.Base64;
 
@@ -25,12 +29,42 @@ public class LoginUserController {
 	 * @param request
 	 * @return
 	 */
-	@ResponseBody
 	@RequestMapping(value = "/getAccount",method = RequestMethod.GET)
-	public JsonResult getAccount(HttpServletRequest request,@RequestParam("mobile") String mobile){
+	@ResponseBody
+	public JsonResult getAccount(HttpServletRequest request,@RequestParam("mobile")String mobile){
 		JsonResult result = new JsonResult(true);
 		WUser user = wUserService.getWUserByMobile(Base64.decodeToString(mobile));
 		result.appendData("data", user);
 		return result;
+	}
+	/**
+	 * 获取用户登录信息
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/redirect/account",method = RequestMethod.GET)
+	public String getAccounts(HttpServletRequest request){
+		UserSessionBean userSessionBean = (UserSessionBean)request.getSession().getAttribute(Constants.TZDR_USER_SESSION);  //获取用户账户信息
+		StringBuffer url = new StringBuffer();
+		url.append(request.getParameter("url"));
+		if(userSessionBean != null){
+			byte[] b = null;  
+	        String s = null;
+    		String mobile = userSessionBean.getMobile();
+    		try {
+				b = mobile.getBytes("utf-8");
+				if (b != null) {  
+					s = Base64.encodeToString(b);
+					url.append("?o="+s+"&check=0"); 
+			   }else{
+				   url.append("?check=1");
+			   }
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}  
+		}else{
+			url.append("?check=1");
+		}
+			return "redirect:"+url.toString()+"";
 	}
 }
