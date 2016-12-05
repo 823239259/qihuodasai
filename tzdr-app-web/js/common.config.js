ffe80a67521a5ff8295601ba5fe845aabc8da4db
丽丽 <@DESKTOP-GD16PRS>
2016-12-05 17:33:03 +0800

    修改配置

--- tzdr-app-web/js/common.config.js
+++ tzdr-app-web/js/common.config.js
@@ -1,4 +1,3 @@
-var model = "live";
 var tradeSocketUrl = "";
 var tradeSocketModelUrl = "";
 var tradeSocketVersion = "";
@@ -21,60 +20,16 @@ var tradeWebSocketIsMock = localStorage.getItem("isMock");
 /**
  * 交易配置配置
  */
-
-function tradeConfig(){
-	this.TradeSocketUrl = "ws://139.224.24.206:6066";//ws://trade.vs.com:6060
-	this.TradeSocketModelUrl = "ws://139.224.24.206:6066";
-//	this.TradeSocketUrl = "ws://139.224.24.206:6066";//"ws://139.196.228.143:6066
-//	this.TradeSocketModelUrl = "ws://139.224.24.206:6066";
-	this.tradeSocketVersion = "2.0.0";
-	this.tradeAppVersion = "2.0.0";
-	return this;
-}
-
-function TradeConfigTest(){ 
-//	this.TradeSocketUrl = "ws://139.224.24.206:6066";//"ws://139.196.228.143:6066
-//	this.TradeSocketModelUrl = "ws://139.224.24.206:6066";
-	this.TradeSocketUrl = "ws://139.224.24.206:6067";//"ws://139.196.228.143:6066
-	this.TradeSocketModelUrl = "ws://139.224.24.206:6067";
-	this.tradeSocketVersion = "2.0.0";
-	this.tradeAppVersion = "2.0.0";
-	return this; 
-}
-
-function MarketConfig(){
-	this.MarketSocketUrl = "ws://quote.vs.com:9002";
-	this.username = "13677622344";
-	this.password = "a123456";
-	return this;
-}
-function MarketConfigTest(){
-	this.MarketSocketUrl = "ws://quote.vs.com:9002";
-	this.username = "13677622344";
-	this.password = "a123456";
-	return this;
-}
-
 loadConfig();
 /**
  * 初始加载交易行情配置数据
  */
 function loadConfig(){ 
-	if(model == "dev"){
-		tradeWebSocketConfig = TradeConfigTest();
-		marketSocketConfig = MarketConfigTest();
-	}else if(model == "live"){
-		tradeWebSocketConfig = tradeConfig(); 
-		marketSocketConfig = MarketConfig();
-	}
-	marketSocketUrl = marketSocketConfig.MarketSocketUrl;
-	marketUserName = marketSocketConfig.username;
-	marketPassword = marketSocketConfig.password;
-	setTradeWebSocketUrlConfig();
-}
-function setTradeWebSocketUrlConfig(){
-	tradeSocketUrl = tradeWebSocketConfig.TradeSocketUrl;
-	tradeSocketModelUrl = tradeWebSocketConfig.TradeSocketModelUrl;
-	tradeSocketVersion = tradeWebSocketConfig.tradeSocketVersion;
-	tradeAppVersion = tradeWebSocketConfig.tradeAppVersion;
+	marketSocketUrl = tzdr.constants.MarketSocketUrl;
+	marketUserName = tzdr.constants.MarketUsername;
+	marketPassword = tzdr.constants.MarketPassword;
+	tradeSocketUrl = tzdr.constants.TradeSocketUrl;;
+	tradeSocketModelUrl = tzdr.constants.TradeSocketModelUrl;
+	tradeSocketVersion = tzdr.constants.tradeSocketVersion;
+	tradeAppVersion = tzdr.constants.tradeAppVersion;
 }
