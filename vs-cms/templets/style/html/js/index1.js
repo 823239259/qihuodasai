088365883d05328b2e3457e640ec0f0b854765c8
丽丽 <@DESKTOP-GD16PRS>
2016-12-05 17:46:23 +0800

    增加文件

---
+++ vs-cms/templets/style/html/js/index1.js
@@ -0,0 +1,96 @@
+/**
+ * Created by 丽丽 on 2016/10/21.
+ */
+var url=window.location.href;
+var param = initLoadParam("o");
+var check = initLoadParam("check");
+$(document).ready(function () {
+        //$("#login").on("click",function(){
+        //    $("#loginDivContent").css("display","block");
+        //});
+        //$("#close").on("click",function(){
+        //    $("#loginDivContent").css("display","none");
+        //});
+        $("#login").bind("click",function(){
+            var urls = url.split("?");
+            var loginUrl = url;
+            if(urls.length > 0){
+                loginUrl = urls[0];
+            }
+            location.href ="http://test.www.vs.com/user/redirectVsNet?url="+loginUrl;
+        });
+     window.onload = loadUserInfo;
+});
+function initLoadParam(name){
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r!=null) return unescape(r[2]); return null; //返回参数值
+}
+function loadUserInfo(){
+    if(check == undefined || check == null){
+        location.href = "http://test.www.vs.com/login/user/redirect/account?url="+url;
+        return ;
+    }
+    var mobile = null;
+    if(param != null){
+        $.ajax({
+            type:"get",
+            url:"http://test.www.vs.com/login/user/getAccount",
+            data:{
+                mobile:param
+            },
+            dataType:'json',//服务器返回json格式数据
+            success:function(result){
+                var data = result.data.data;
+                if(data != null){
+                    mobile = data.mobile;
+                    if(mobile != null){
+                        console.log(mobile);
+                        mobile=mobile.substring(0,3)+"****"+mobile.substring(7,11);
+                        $("#login").html("欢迎您，<span>"+mobile+"</span>");
+                        $("#registerALL").html("<a href='javascript:void(0)' id='signOut'>退出</a>");
+                        $("#personalCenter").html("我的账户");
+                    }
+                }
+            }
+        });
+    }
+}
+function getLocalTime(nS) {
+    return  new Date(parseInt(nS) * 1000).Format("yyyy-MM-dd hh:mm:ss");
+}
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+function queryData(url,params,success,error){
+    var funUrl="http://www.vs.com/";
+    url=funUrl+url;
+    var success = arguments[2]?arguments[2]:function(){};
+    var error = arguments[3]?arguments[3]:function(){};
+    $.ajax({
+        url:url,
+        type:"get",
+        dataType:"json",
+        data:params,
+        success: function (res) {
+            if(res.success==true){
+                success(res.data.data);
+            }
+        },
+        error: function (res) {
+            error(res);
+        }
+    })
+}
---
+++ vs-cms/templets/style/html/js/scroll.js
@@ -0,0 +1,53 @@
+/**
+ * Created by ÀöÀö on 2016/11/28.
+ */
+(function($){
+    var status = false;
+    $.fn.scrollQ = function(options){
+        var defaults = {
+            line:2,
+            scrollNum:2,
+            scrollTime:10000
+        }
+        var options=jQuery.extend(defaults,options);
+        var _self = this;
+        return this.each(function(){
+            $("li",this).each(function(){
+                //$(this).css("display","none");
+            })
+            $("li:lt("+options.line+")",this).each(function(){
+                //$(this).css("display","block");
+            })
+            function scroll() {
+                for(i=0;i<options.scrollNum;i++) {
+                    var start=$("li:first",_self);
+                    //start.fadeOut(100);
+                    start.animate({
+                        "marginTop":"10px"
+                    });
+                    //start.css("display","none");
+                    start.appendTo(_self);
+                    $("li:eq("+(options.line-1)+")",_self).each(function(){
+                        //console.log(options.line-1);
+                        //$(this).fadeIn(500);
+                        //$(this).css("display","block");
+                        $(this).animate({
+                            "marginTop":"-30px"
+                        },1500);
+
+                    })
+                }
+            }
+            var timer;
+            scroll();
+            timer = setInterval(scroll,options.scrollTime);
+            _self.bind("mouseover",function(){
+                clearInterval(timer);
+            });
+            _self.bind("mouseout",function(){
+                timer = setInterval(scroll,options.scrollTime);
+            });
+
+        });
+    }
+})(jQuery);
