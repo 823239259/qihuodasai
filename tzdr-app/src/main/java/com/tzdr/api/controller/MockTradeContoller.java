3bade1238f626644978f5244ae1a849e1af98c54
gaochao <gaochao@vs.com>
2017-01-18 14:11:14 +0800

    还原成api升级前代码

--- tzdr-app/src/main/java/com/tzdr/api/controller/MockTradeContoller.java
+++ tzdr-app/src/main/java/com/tzdr/api/controller/MockTradeContoller.java
@@ -1,19 +1,16 @@
 package com.tzdr.api.controller;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.tzdr.api.constants.ResultStatusConstant;
 import com.tzdr.api.support.ApiResult;
 import com.tzdr.business.cms.cpp.MockTradeAccountService;
 
-import jodd.util.StringUtil;
 
 @Controller
 @RequestMapping(value = "/mock/trade")
@@ -30,9 +27,6 @@ public class MockTradeContoller {
 	@ResponseBody
 	@RequestMapping(value = "/openMock")
 	public ApiResult openMock(HttpServletRequest request,@RequestParam("username")String username,@RequestParam("password")String password){
-		if(StringUtil.isBlank(username) || StringUtil.isBlank(password)){
-			return new ApiResult(false,ResultStatusConstant.AUTH_PARAMS_ERROR,"param is null");
-		}
 		boolean flag = mockTradeAccountService.openMockAccount(username, password);
 		if(flag){
 			return new ApiResult(true,ResultStatusConstant.SUCCESS,"open mock success");
@@ -47,13 +41,9 @@ public class MockTradeContoller {
 	@ResponseBody
 	@RequestMapping(value = "/updateMock",method = RequestMethod.POST)
 	public ApiResult updateMock(HttpServletRequest request,@RequestParam("username")String username,@RequestParam("password")String password){
-		if(StringUtil.isBlank(username) || StringUtil.isBlank(password)){
-			return new ApiResult(false,ResultStatusConstant.AUTH_PARAMS_ERROR,"param is null");
-		}
 		boolean flag = mockTradeAccountService.updateMockAccount(username, password);
-		ApiResult apiResult = new ApiResult();
 		if(flag){
-			apiResult.setSuccess(false);return new ApiResult(true,ResultStatusConstant.SUCCESS,"update mock success");
+			return new ApiResult(true,ResultStatusConstant.SUCCESS,"update mock success");
 		}else{
 			return new ApiResult(false,ResultStatusConstant.FAIL,"update mock fail");
 		}
