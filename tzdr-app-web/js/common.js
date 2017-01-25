680f6eef43f0dd1370128c3c157dda478c61a277
hubo <hubo@invtool.com>
2017-01-25 15:05:43 +0800

    调整配置文件

--- tzdr-app-web/js/common.js
+++ tzdr-app-web/js/common.js
@@ -256,8 +256,7 @@
 var tzdr = {
 	// 系统常量
 	constants:{
-		api_domain:"http://test.api.vs.com/", 
-//		api_domain:"http://192.168.0.93:8080/tzdr-app/",
+		api_domain:"http://api.vs.com/", 
 		//图片地址
 		base_images_url:'http://manage.vs.com/',
 		//token
