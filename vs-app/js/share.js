bd0ebd77b8be9cc7a2a2a025f39c462988db93b1
黑黑哈哈 <972399435@qq.com>
2017-02-06 16:51:22 +0800

    修复分享的bug

--- vs-app/js/share.js
+++ vs-app/js/share.js
@@ -84,9 +84,8 @@
 			shareBts.push({title:'微信好友',s:ss,x:'WXSceneSession'}));
 			ss=shares['qq'];
 			ss&&ss.nativeClient&&shareBts.push({title:'QQ',s:ss});
-			
 			// 弹出分享列表
-			shareBts.length>0?plus.nativeUI.actionSheet({title:'分享链接',cancel:'取消',buttons:shareBts},function(e){
+			plus.nativeUI.actionSheet({title:'分享链接',cancel:'取消',buttons:shareBts},function(e){
 				(e.index>0)&&shareAction(shareBts[e.index-1],true);
-			}):plus.nativeUI.alert('暂不支持分享功能');
+			});
 		}
