eadd67436ea66c107c21a06d29b51833396aafca
hubo <hubo@invtool.com>
2017-02-22 11:31:45 +0800

    格式化代码，便于维护

--- vs-app/js/trade.connection.js
+++ vs-app/js/trade.connection.js
@@ -6,7 +6,7 @@ var socketUrl = null;
 /**
  * 用户名
  */
-var username =localStorage.getItem("trade_account");
+var username = localStorage.getItem("trade_account");
 /**
  * 密码
  */
@@ -51,26 +51,26 @@ var isConnectionError = true;
  * 设置登录状态
  * @param flag
  */
-function setIsLogin(flag){
+function setIsLogin(flag) {
 	isLogin = flag;
 }
 /**
  * 验证是否登录
  */
-function vadationIsLogin(){
-	if(!isLogin){
-		alertProtype("你还未登录,请先登录","提示",Btn.confirmedAndCancle(),openLogin);
+function vadationIsLogin() {
+	if(!isLogin) {
+		alertProtype("你还未登录,请先登录", "提示", Btn.confirmedAndCancle(), openLogin);
 		return false;
-	} 
+	}
 	return true;
 }
 /**
  * 验证是否登录
  */
-function vadationIsLoginMuiTip(){
-	if(isLogin){
+function vadationIsLoginMuiTip() {
+	if(isLogin) {
 		return true;
-	}else{
+	} else {
 		tip("未登录,请先登录");
 		return false
 	}
@@ -78,36 +78,36 @@ function vadationIsLoginMuiTip(){
 /**
  * 更新连接状态
  */
-function changeConnectionStatus(){
-	if(socket == null){
+function changeConnectionStatus() {
+	if(socket == null) {
 		connectionStatus = false;
-	}else{
+	} else {
 		connectionStatus = true;
 	}
 }
 /**
  * 交易连接
  */
-function tradeConnection(){  
-	if(socketUrl.length > 0){
-		if(mui.checkNetwork()){
-			socket = new WebSocket(socketUrl); 
-			if(socket.readyState == 0){
+function tradeConnection() {
+	if(socketUrl.length > 0) {
+		if(mui.checkNetwork()) {
+			socket = new WebSocket(socketUrl);
+			if(socket.readyState == 0) {
 				isConnectionError = false;
-			}else{
+			} else {
 				isConnectionError = true;
 			}
-		}else{
+		} else {
 			isConnectionError = true;
 			plus.nativeUI.closeWaiting();
-			clearInterval(tradeIntervalId); 
+			clearInterval(tradeIntervalId);
 		}
 	}
 }
 /**
  * 交易连接断开的处理
  */
-function tradeConnectionClose(){
+function tradeConnectionClose() {
 	Trade.doLoginOut(username);
 	socket == null;
 	changeConnectionStatus();
@@ -116,7 +116,7 @@ function tradeConnectionClose(){
 /**
  * 本地退出登录
  */
-function loginOut(){ 
+function loginOut() {
 	localStorage.removeItem("trade_account");
 	localStorage.removeItem("trade_password");
 	localStorage.removeItem("isMock");
@@ -134,27 +134,27 @@ function loginOut(){
  * @param {Object} account 登录用户名
  * @param {Object} password 登录密码
  */
-function loginCache(account,password){
-	localStorage.setItem("trade_account",account);
-	localStorage.setItem("trade_password",password);
-	localStorage.setItem("trade_endLoginAccount",account);
-	localStorage.setItem("trade_endLoginPassword",password);
+function loginCache(account, password) {
+	localStorage.setItem("trade_account", account);
+	localStorage.setItem("trade_password", password);
+	localStorage.setItem("trade_endLoginAccount", account);
+	localStorage.setItem("trade_endLoginPassword", password);
 }
 /**
  * 交易登录
  * @param account
  * @param password
  */
-function tradeLogin(){
-	if(username != null && password != null){
+function tradeLogin() {
+	if(username != null && password != null) {
 		initTradeConnect();
-		var loginInterval = setInterval(function(){
-			if(connectionStatus && isLogin){
-				loginCache(username , password);
+		var loginInterval = setInterval(function() {
+			if(connectionStatus && isLogin) {
+				loginCache(username, password);
 				clearInterval(loginInterval);
 			}
-		}, 500); 
-	}else{
+		}, 500);
+	} else {
 		plus.nativeUI.closeWaiting();
 	}
 }
@@ -162,7 +162,7 @@ function tradeLogin(){
  * 交易登录退出
  * @param account
  */
-function tradeLoginOut(){
+function tradeLoginOut() {
 	Trade.doLoginOut(username);
 	loginOut();
 }
@@ -170,11 +170,11 @@ function tradeLoginOut(){
  * 根据交易模式设置交易配置信息
  * @param ismock
  */
-function setTradeConfig(ismock){
+function setTradeConfig(ismock) {
 	setTradeWebSocketIsMock(ismock);
-	if(tradeWebSocketIsMock == 0){
+	if(tradeWebSocketIsMock == 0) {
 		socketUrl = tradeWebsocketUrl;
-	}else if(tradeWebSocketIsMock == 1){
+	} else if(tradeWebSocketIsMock == 1) {
 		socketUrl = tradeWebsocketModelUrl;
 	}
 }
@@ -182,115 +182,113 @@ function setTradeConfig(ismock){
  * 交易初始化加载
  */
 function initLoad() {
-	    if(isConnectionError){
-	    	 plus.nativeUI.closeWaiting();
-	    	clearInterval(tradeIntervalId);
-			alertProtype("交易服务器连接错误,请检查网络连接","提示",Btn.confirmed(),null,null,null);
-			return;
-	    }
-		socket.onopen = function() {   
-			/*layer.closeAll();*/ 
-			Trade.doLogin(username , password,tradeWebSocketIsMock,tradeWebSocketVersion,Source); 
-			//更新交易连接状态
-			changeConnectionStatus();
-		}
-		socket.onmessage = function(evt) {
-			handleData(evt);
-		} 
-		socket.onclose = function() {
-			clearInterval(tradeIntervalId);
-			socket = null;
-			//更新交易连接状态
-			changeConnectionStatus();
-			//不是手动登出，则重连交易服务器
-			if(loginFail == false){ 
-				//交易连接断开重连
-				tradeReconnect();
-			}else{
-				if(anotherPlace && loginFail){
-					alertProtype("您的账号在另一地点登录，您被迫下线。如果不是您本人操作，那么您的密码很可能已被泄露，建议您及时致电：400-852-8008","下线提示",Btn.confirmed(),null,null,null);
-					//clearLocalCacheData();
-					loginOut();
-				}
+	if(isConnectionError) {
+		plus.nativeUI.closeWaiting();
+		clearInterval(tradeIntervalId);
+		alertProtype("交易服务器连接错误,请检查网络连接", "提示", Btn.confirmed(), null, null, null);
+		return;
+	}
+	socket.onopen = function() {
+		/*layer.closeAll();*/
+		Trade.doLogin(username, password, tradeWebSocketIsMock, tradeWebSocketVersion, Source);
+		//更新交易连接状态
+		changeConnectionStatus();
+	}
+	socket.onmessage = function(evt) {
+		handleData(evt);
+	}
+	socket.onclose = function() {
+		clearInterval(tradeIntervalId);
+		socket = null;
+		//更新交易连接状态
+		changeConnectionStatus();
+		//不是手动登出，则重连交易服务器
+		if(loginFail == false) {
+			//交易连接断开重连
+			tradeReconnect();
+		} else {
+			if(anotherPlace && loginFail) {
+				alertProtype("您的账号在另一地点登录，您被迫下线。如果不是您本人操作，那么您的密码很可能已被泄露，建议您及时致电：400-852-8008", "下线提示", Btn.confirmed(), null, null, null);
+				//clearLocalCacheData();
+				loginOut();
 			}
 		}
+	}
 }
 /**
  * 重新连接交易服务器
  */
-function tradeReconnect(){
+function tradeReconnect() {
 	//layer.msg('交易连接断开,正在重新连接...', {icon: 16});
-	if(socket == null){
+	if(socket == null) {
 		initTrade();
 	}
 }
 /**
  * 初始化交易
  */
-	function initTradeConnect(){
-		/**
-		 * 交易连接 -->trade.connection
-		 */
-		tradeConnection();
-		/**
-		 * 交易数据初始化加载 --> trade.connections
-		 */
-		initLoad();
-		tradeIntervalId = setInterval(function(){
-				/*layer.msg('正在连接交易服务器...', {icon: 16});*/
-				if(connectionStatus){
-					/*layer.msg('交易服务器连接成功', {icon: 4});*/
-					clearInterval(tradeIntervalId);
-				}
-			}
-		, 2000);
-	}
-function initTrade(){
+function initTradeConnect() {
+	/**
+	 * 交易连接 -->trade.connection
+	 */
+	tradeConnection();
+	/**
+	 * 交易数据初始化加载 --> trade.connections
+	 */
+	initLoad();
+	tradeIntervalId = setInterval(function() {
+		/*layer.msg('正在连接交易服务器...', {icon: 16});*/
+		if(connectionStatus) {
+			/*layer.msg('交易服务器连接成功', {icon: 4});*/
+			clearInterval(tradeIntervalId);
+		}
+	}, 2000);
+}
+
+function initTrade() {
 	/**
 	 * 交易登录（初始化） -->trade.connection
 	 */
 	tradeLogin();
 }
 
-
 /**
  * 获取版本信息
  */
-function getVersion(){ 
-	$.ajax({ 
-		url:tzdr.constants.api_domain+"/socket/config/getVersions",
-		type:"post", 
-		data:{
-			appVersions:appVersion
+function getVersion() {
+	$.ajax({
+		url: tzdr.constants.api_domain + "/socket/config/getVersions",
+		type: "post",
+		data: {
+			appVersions: appVersion
 		},
-		timeout:5000,
-		success:function(result){
-			if(result.success){
-				var data = result.data; 
+		timeout: 5000,
+		success: function(result) {
+			if(result.success) {
+				var data = result.data;
 				tradeWebsocketUrl = data.socketUrl;
 				tradeWebSocketVersion = data.socketVersion;
 				tradeWebsocketModelUrl = data.socketModelUrl;
-				isGetVersion = true; 
-			} 
-		} ,
-		error:function(result){
-		}
-	});  
+				isGetVersion = true;
+			}
+		},
+		error: function(result) {}
+	});
 }
 /**
  * 验证socket版本是否获取成功
  */
-function validateIsGetVersion(){
+function validateIsGetVersion() {
 	var i = 0;
-	var initIsGetVersion = setInterval(function(){
+	var initIsGetVersion = setInterval(function() {
 		i++;
-//		console.log(i);
-		if(isGetVersion == false){
-			if(i > 50){ 
+		//		console.log(i);
+		if(isGetVersion == false) {
+			if(i > 50) {
 				isGetVersion = true;
 			}
 		}
-		if(isGetVersion == true){
+		if(isGetVersion == true) {
 			initSocketTrade();
 			clearInterval(initIsGetVersion);
 		}
