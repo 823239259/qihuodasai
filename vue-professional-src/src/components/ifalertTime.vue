4e050290f56b353ec25b944157577bfa533788a5
wuxuanfeng <527140631@qq.com>
2017-09-13 16:34:00 +0800

    条件单修改 添加miniTikeSize

--- vue-professional-src/src/components/ifalertTime.vue
+++ vue-professional-src/src/components/ifalertTime.vue
@@ -1,18 +1,20 @@
 <template>
 	<div id="ifalert" v-if="isshow">
 		<alert title="提示" :line1="tipsAlert" :objstr="sendMsg" ref="alert"></alert>
+		<tipsDialog :msg="msgTips" ref="dialog"></tipsDialog>
 		<div class="ifalert_box">
 			<ul class="selectbar">
-				<li class="fontgray fl selected" @tap="selection">时间条件</li>
+				<li class="fontgray fl selected">时间条件</li>
 			</ul>
 			<ul class="content">
 				<li>
 					<ol>
 						<li class="fontgray">合约</li>
 						<li>
-							<select name="contract" class="selectlong fontwhite" v-model="selectTimeId">
+							<input type="text" v-model="selectTimeId" class="selectlong fontwhite" disabled />
+							<!--<select name="contract" class="selectlong fontwhite" v-model="selectTimeId">
 								<option v-for="v in parameters" :value="v.CommodityNo+v.MainContract">{{v.CommodityName}}</option>
-							</select>
+							</select>-->
 						</li>
 					</ol>
 				</li>
@@ -81,6 +83,7 @@
 </template>
 
 <script>
+	import tipsDialog from './tipsDialog.vue'
 	import alert from './Tradealert.vue'
 	export default {
 		name: 'ifalert',
@@ -114,11 +117,12 @@
 				timeBuyOrSell:0,
 				additionValue:'',
 				tipsMsg: '',
-				str: ''
+				str: '',
+				msg: ''
 			}
 		},
 		props: ['objstr'],
-		components: {alert},
+		components: {alert, tipsDialog},
 		computed:{
 			height1(){
 				return $('#ifalert>div').css('height').slice(0,-2);
@@ -139,18 +143,24 @@
 				return this.$store.state.tradeSocket;
 			},
 			objstrParms: function(){
-				return this.objstr;
+				if(this.objstr) return JSON.parse(this.objstr);
 			},
 			tipsAlert: function(){
 				return this.tipsMsg;
 			},
+			msgTips: function(){
+				return this.msg;
+			},
 			sendMsg: function(){
 				if(this.str) return JSON.stringify(this.str);
 			},
+			miniTikeSize(){
+				return this.orderTemplist[this.objstrParms.CommodityNo].MiniTikeSize;
+			}
 		},
 		watch:{
 			objstrParms:function(n,o){
-				let sb= JSON.parse(n);
+				let sb= n;
 				this.selectTimeId = sb.CommodityNo+sb.ContractNo;
 				let time00 = sb.TimeTriggerPoint.split(' ')[1];
 				this.time = time00.split(':')[0]+':'+time00.split(':')[1];
@@ -173,25 +183,24 @@
 					this.timeAddtionPrice = '';
 				}else{
 					$(".additionalTime").removeAttr("disabled");
-					let size = this.orderTemplist[JSON.parse(this.objstrParms).CommodityNo].DotSize;
-					this.timeAddtionPrice = parseFloat(this.templateList[JSON.parse(this.objstrParms).CommodityNo].LastPrice).toFixed(size);
+					let size = this.orderTemplist[this.objstrParms.CommodityNo].DotSize;
+					this.timeAddtionPrice = parseFloat(this.templateList[this.objstrParms.CommodityNo].LastPrice).toFixed(size);
 				}
 			}
 		},
 		methods:{
-			selection:function(e){
-				if(e.target.innerHTML == '价格条件'){
-					this.ifshow=true;
-					$('#ifalert>div').css('height',this.height1+'px');
-				}else{
-					this.ifshow=false;
-					$('#ifalert>div').css('height',this.height2+'px');
-				}
-			},
 			close: function() {
 				this.isshow = false;
 			},
 			confirm: function() {
+				if(this.timeAddtionPrice){
+					var d2 = this.timeAddtionPrice % this.miniTikeSize;
+					if(d2 >= 0.000000001 && parseFloat(this.miniTikeSize-d2) >= 0.0000000001){
+						this.$refs.dialog.isShow = true;
+						this.msg = '输入附加价格不符合最小变动价，最小变动价为：' + this.miniTikeSize;
+						return;
+					}
+				}
 				this.$refs.alert.isshow = true;
 				this.tipsMsg = '是否修改价格条件单？';
 				function getNowFormatDate() {
@@ -210,7 +219,7 @@
 				}
 				let dateTime= getNowFormatDate()+' '+this.time+':'+new Date().getSeconds();
 //				this.isshow = false;
-				let mmp = JSON.parse(this.objstrParms);
+				let mmp = this.objstrParms;
 				let b={
 						"Method":'ModifyCondition',
 						"Parameters":{
@@ -240,8 +249,8 @@
 							
 						}
 					};
-				this.tradeSocket.send(JSON.stringify(b));	
-				
+//				this.tradeSocket.send(JSON.stringify(b));	
+				this.str = b;
 			}
 		},
 		mounted:function(){
