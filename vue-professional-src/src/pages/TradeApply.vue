3572ed64ca386c6af9d094e1bb53819efe70ea14
wuxuanfeng <527140631@qq.com>
2017-09-20 11:19:58 +0800

    开户页面

--- vue-professional-src/src/pages/TradeApply.vue
+++ vue-professional-src/src/pages/TradeApply.vue
@@ -1,143 +1,170 @@
 <template>
 	<div id="applydetail">
+		<tipsDialog :msg="msgTips" ref="dialog"></tipsDialog>
 		<topbar title='开户'></topbar>
 		<kf title="操盘细则" type="1" status="1" @tap.native="toTradersRules"></kf>
 		<kf title="客服"></kf>
-		<!--tab s-->
-		<div class="title">
-			<ul>
-				<li :class="{current: isselected}">
-					<span>开户申请</span>
-				</li>
-				<li>
-					<span>开户记录</span>
-				</li>
-			</ul>
-		</div>
-		<!--tab e-->
-		
-		<!--轮播 s-->
-		<div id="slider" class="mui-slider">
-			<div class="mui-slider-group">
-				<!--<div class="mui-slider-item mui-slider-item-duplicate">
-					<a href="#"><img :src="bannerList[bannerList.length - 1].imgPath"></a>
-				</div>-->
-				<!--第一个内容区容器-->
-				<template v-for="k in bannerList">
-					<div class="mui-slider-item">
-						<a href="#"><img :src="k.imgPath" /></a>
-					</div>
-				</template>
-				<!--<div class="mui-slider-item mui-slider-item-duplicate">
-					<a href="#"><img :src="bannerList[0].imgPath"></a>
-				</div>-->
+		<div class="main">
+			<!--tab s-->
+			<div class="title">
+				<ul>
+					<template v-for="(k, index) in tabList">
+						<li :class="{current: isselected == index}" @tap="tabEvent(index)">
+							<span>{{k.title}}</span>
+						</li>
+					</template>
+				</ul>
 			</div>
-		</div>
-		<!--轮播 e-->
-		
-		<!--选择操盘保证金-->
-		<div class="margin_trade">
-			<p class="fontwhite">选择操盘保证金（金额越多，可持仓手数越多）</p>
-			<ul>
-				<li class="fl" @tap='choose'>
-					<mr num='3000'></mr>
-				</li>
-				<li class="fl" @tap='choose'>
-					<mr num='6000'></mr>
-				</li>
-				<li class="fl" @tap='choose'>
-					<mr num='10000'></mr>
-				</li>
-				<li class="fl" @tap='choose'>
-					<mr num='12000'></mr>
-				</li>
-				<li class="fl" @tap='choose'>
-					<mr num='15000'></mr>
-				</li>
-				<li class="fl" @tap='choose'>
-					<mr num='50000'></mr>
-				</li>
-				<li class="fl" @tap='choose'>
-					<mr num='100000'></mr>
-				</li>
-				<li class="fl" @tap='choose'>
-					<mr num='200000'></mr>
-				</li>
-			</ul>
-		</div>
-		<!--操盘保证金及资金显示-->
-		<div class="margin_trade_show">
-			<ul>
-				<li class="fontgray">操盘保证金：<span class="fontwhite">{{chooseType | moneytype(chooseType)}}元</span></li>
-				<li class="fontgray">总操盘资金：<span class="fontwhite">{{traderTotal | moneytype(chooseType)}}美元</span></li>
-				<li class="fontgray">亏损平仓线：<span class="fontwhite">{{lineLoss | moneytype(chooseType)}}美元</span></li>
-			</ul>
-		</div>
-		<!--按钮-->
-		<ul class="bottom">
-			<li class="fontyellow">
-				总计：<span><span class="fontyellow">{{chooseType | moneytype(chooseType)}}元</span></span>
-			</li>
-			<li>
-				<bbtn name='马上开户' @tap.native='toConfirm'></bbtn>
-			</li>
-		</ul>
-		<!--可交易品种-->
-		<div class="notice">
-			<p class="fontwhite">可交易品种（一个账号可同时交易17种期货产品）</p>
-			<p class="fontgray">
-				<span class="fontyellow">注意：</span>请不要在交易时间外持单，以免被系统强制平仓
-			</p>
-		</div>
-		<!--列表-->
-		<div class="datalist">
-			<ul class="list_head">
-				<li class="odd">
-					<span>期货产品</span>
-				</li>
-				<li class="even">
-					<span>交易手续费</span>
-					<span>单边</span>
-				</li>
-				<li class="even">
-					<span>初始持仓手数</span>
-					<span>只交易该品种时，最大持仓</span>
-				</li>
-				<li class="odd">
-					<span>交易时间</span>
-				</li>
-				<li class="odd">
-					<span>简介</span>
-				</li>
-			</ul>
-			<template v-for="k in temp.contractList">
-				<ul class="list_cont">
-					<li class="even">
-						<span>{{k.tradeType | cnname}}</span>
-						<span>{{k.mainContract}}</span>
+			<!--tab e-->
+			
+			<div class="cont" v-if="show">
+				<!--轮播 s-->
+				<div id="slider" class="mui-slider">
+					<div class="mui-slider-group">
+						<!--<div class="mui-slider-item mui-slider-item-duplicate">
+							<a href="#"><img :src="bannerList[bannerList.length - 1].imgPath"></a>
+						</div>-->
+						<template v-for="k in bannerList">
+							<div class="mui-slider-item">
+								<a href="#"><img :src="k.imgPath" /></a>
+							</div>
+						</template>
+						<!--<div class="mui-slider-item mui-slider-item-duplicate">
+							<a href="#"><img :src="bannerList[0].imgPath"></a>
+						</div>-->
+					</div>
+				</div>
+				<!--轮播 e-->
+				
+				<!--选择操盘保证金-->
+				<div class="margin_trade">
+					<p class="fontwhite">选择操盘保证金（金额越多，可持仓手数越多）</p>
+					<ul>
+						<li class="fl" @tap='choose'>
+							<mr num='3000'></mr>
+						</li>
+						<li class="fl" @tap='choose'>
+							<mr num='6000'></mr>
+						</li>
+						<li class="fl" @tap='choose'>
+							<mr num='10000'></mr>
+						</li>
+						<li class="fl" @tap='choose'>
+							<mr num='12000'></mr>
+						</li>
+						<li class="fl" @tap='choose'>
+							<mr num='15000'></mr>
+						</li>
+						<li class="fl" @tap='choose'>
+							<mr num='50000'></mr>
+						</li>
+						<li class="fl" @tap='choose'>
+							<mr num='100000'></mr>
+						</li>
+						<li class="fl" @tap='choose'>
+							<mr num='200000'></mr>
+						</li>
+					</ul>
+				</div>
+				<!--操盘保证金及资金显示-->
+				<div class="margin_trade_show">
+					<ul>
+						<li class="fontgray">操盘保证金：<span class="fontwhite">{{chooseType | moneytype(chooseType)}}元</span></li>
+						<li class="fontgray">总操盘资金：<span class="fontwhite">{{traderTotal | moneytype(chooseType)}}美元</span></li>
+						<li class="fontgray">亏损平仓线：<span class="fontwhite">{{lineLoss | moneytype(chooseType)}}美元</span></li>
+					</ul>
+				</div>
+				<!--按钮-->
+				<ul class="bottom">
+					<li class="fontyellow">
+						总计：<span><span class="fontyellow">{{chooseType | moneytype(chooseType)}}元</span></span>
 					</li>
-					<li class="odd">
-						<span>{{k.price}}元/手</span>
-					</li>
-					<li class="odd">
-						<span>{{k.shoushu | filtershoushu(chooseType)}}</span>
-					</li>
-					<li class="even">
-						<span>{{k.tradTime}}</span>
-					</li>
-					<li class="even">
-						<span>最小变动单位0.01，跳动一下扣除10美元</span>
+					<li>
+						<bbtn name='马上开户' @tap.native='toConfirm'></bbtn>
 					</li>
 				</ul>
-			</template>
+				<!--可交易品种-->
+				<div class="notice">
+					<p class="fontwhite">可交易品种（一个账号可同时交易17种期货产品）</p>
+					<p class="fontgray">
+						<span class="fontyellow">注意：</span>请不要在交易时间外持单，以免被系统强制平仓
+					</p>
+				</div>
+				<!--列表-->
+				<div class="datalist">
+					<ul class="list_head">
+						<li class="odd">
+							<span>期货产品</span>
+						</li>
+						<li class="even">
+							<span>交易手续费</span>
+							<span>单边</span>
+						</li>
+						<li class="even">
+							<span>初始持仓手数</span>
+							<span>只交易该品种时，最大持仓</span>
+						</li>
+						<li class="odd">
+							<span>交易时间</span>
+						</li>
+						<li class="odd">
+							<span>简介</span>
+						</li>
+					</ul>
+					<template v-for="k in temp.contractList">
+						<ul class="list_cont">
+							<li class="even">
+								<span>{{k.tradeType | cnname}}</span>
+								<span>{{k.mainContract}}</span>
+							</li>
+							<li class="odd">
+								<span>{{k.price}}元/手</span>
+							</li>
+							<li class="odd">
+								<span>{{k.shoushu | filtershoushu(chooseType)}}</span>
+							</li>
+							<li :class="{odd: k.tradTime.length < 25, even: k.tradTime.length >= 25}">
+								<span>{{k.tradTime}}</span>
+							</li>
+							<li class="even">
+								<span>最小变动单位0.01，跳动一下扣除10美元</span>
+							</li>
+						</ul>
+					</template>
+				</div>
+				<!--提交申请-->
+				<ul class="info">
+					<li class="fontgray">提交申请表示阅读并同意 <span class="fontwhite" @tap="toAgreement"> 《国际期货操盘合作协议》 </span> </li>
+					<li class="fontgray">
+						客服咨询： <span class="fontwhite"> 400-852-8008 </span>
+					</li>
+				</ul>
+			</div>
+			<div class="page_cont" v-else="!show">
+				<ul>
+					<template v-for="key in tradeList">
+						<li @tap="toDetails" :type="key.stateType" :id="key.id">
+							<div class="record_title">
+								<span :class="{yellow: key.stateType == 1, blue: key.stateType == 4, grey: key.stateType == 5, deepblue: key.stateType == 6}">{{key.stateType | operateState}}</span>
+								<i class="icon_arrow fr"></i>
+								<span class="fr">{{key.appTime | getTime('yyyy-MM-dd HH:mm')}}</span>
+							</div>
+							<div class="record_cont">
+								<p>交易品种：<span>{{key.businessType | varieties}}</span></p>
+								<p>总操盘资金：<span>{{key.traderTotal}}美元</span></p>
+								<p>开仓手数：<span class="spe">{{key.tranLever | operateNumber}}</span></p>
+								<p>亏损平仓线：<span>{{key.lineLoss}}美元</span></p>
+							</div>
+						</li>
+					</template>
+				</ul>
+				<!--无操盘记录-->
+				<div class="empty" id="empty" v-show="emptyShow">
+					<i class="icon_none"></i>
+					<p>您还没有操盘记录</p>
+				</div>
+			</div>
 		</div>
-		<!--提交申请-->
-		<ul class="info">
-			<li class="fontgray">提交申请表示阅读并同意 <span class="fontwhite" @tap="toAgreement"> 《国际期货操盘合作协议》 </span> </li>
-			<li class="fontgray">
-				客服咨询： <span class="fontwhite"> 400-852-8008 </span>
-			</li>
-		</ul>
 	</div>
 </template>
 
@@ -147,21 +174,29 @@
 	import back from '../components/back.vue'
 	import kf from '../components/customerService.vue'
 	import mr from '../components/moneyradio.vue'
+	import tipsDialog from '../components/tipsDialog.vue'
 	export default {
 		name: 'applydetail',
-		components: {
-			topbar,
-			bbtn,
-			back,
-			kf,
-			mr
-		},
+		components: {topbar, bbtn, back, kf, mr, tipsDialog},
 		data() {
 			return {
-				isselected: true,
+				isselected: 0,
+				tabList: [
+					{
+						title: '开户申请'
+					},
+					{
+						title: '开户记录'
+					}
+				],
 				bannerList: [],
 				temp: {},
-				chooseType: 3000
+				show: true,
+				chooseType: 3000,
+				emptyShow: false,
+				tradeList: '',
+				userInfo: '',
+				msg: ''
 			}
 		},
 		created: function(){
@@ -173,11 +208,7 @@
 				timeout: 5000
 			}).then(function(e) {
 				this.temp = e.body.data;
-				console.log(this.temp.contractList);
 				this.temp.contractList.forEach(function(o, i) {
-					var time = o.tradTime.split(',');
-					console.log(time);
-					
 					switch(o.tradeType) {
 						case 0:   //return '富时A50'
 							var arr = [];
@@ -307,7 +338,7 @@
 			}.bind(this), function(e) {});
 		},
 		filters: {
-			filtershoushu:function(arr,chooseType){
+			filtershoushu: function(arr,chooseType){
 				switch(chooseType){
 					case 3000:
 						return arr[0];
@@ -392,7 +423,78 @@
 						return '天然气'
 						break;
 				}
-
+			},
+			varieties: function(e){    //交易品种
+				switch(e) {
+					case 8:
+						return "国际综合";
+						break;
+					case 7:
+						return "恒指期货";
+						break;
+					case 6:
+						return "国际原油";
+						break;
+					case 0:
+						return "富时A50";
+						break;
+				}
+			},
+			operateState: function(e){     //状态
+				switch(e) {
+					case 1:
+						return "开户中";
+						break;
+					case 2:
+						return "申请结算";
+						break;
+					case 3:
+						return "待结算";
+					case 4:
+						return "操盘中";
+						break;
+					case 5:
+						return "开户失败";
+						break;
+					case 6:
+						return "已结算";
+						break;
+				}
+			},
+			operateNumber: function(e){
+				if(!e){
+					return "初始可持仓手数";	
+				}else {
+					return e +"手";	
+				}
+			},
+			getTime: function(e, format) {
+				var t = new Date(e * 1000);
+				var tf = function(i) {
+					return(i < 10 ? '0' : '') + i
+				};
+				return format.replace(/yyyy|MM|dd|HH|mm|ss/g, function(a) {
+					switch(a) {
+						case 'yyyy':
+							return tf(t.getFullYear());
+							break;
+						case 'MM':
+							return tf(t.getMonth() + 1);
+							break;
+						case 'mm':
+							return tf(t.getMinutes());
+							break;
+						case 'dd':
+							return tf(t.getDate());
+							break;
+						case 'HH':
+							return tf(t.getHours());
+							break;
+						case 'ss':
+							return tf(t.getSeconds());
+							break;
+					};
+				});
 			}
 		},
 		computed: {
@@ -401,6 +503,9 @@
 			},
 			environment(){
 				return this.$store.state.environment;
+			},
+			msgTips: function(){
+				return this.msg;
 			},
 			traderTotal: function() {
 				if(this.temp.paramList){
@@ -470,6 +575,14 @@
 			toAgreement: function(){
 				this.$router.push({path: '/agreement'});
 			},
+			tabEvent: function(i){
+				this.isselected = i;
+				if(i == 1){
+					this.show = false;
+				}else{
+					this.show = true;
+				}
+			},
 			getBanner: function(){
 				this.$http.post(this.PATH + '/banner/list', {emulateJSON: true},{
 					params: {
@@ -492,7 +605,7 @@
 					}else{
 						switch (data.code){
 							case '2':
-								this.$children[0].isShow = true;
+								this.$refs.dialog.isShow = true;
 								this.msg = '处理失败';
 								break;
 							default:
@@ -500,7 +613,7 @@
 						}
 					}
 				}.bind(this), function(){
-					this.$children[0].isShow = true;
+					this.$refs.dialog.isShow = true;
 					this.msg = '网络不给力，请稍后再试！';
 				});
 			},
@@ -524,18 +637,80 @@
 						'lineLoss':this.lineLoss
 					}
 				})
+			},
+			toDetails: function(e){
+				var type = $(e.currentTarget).attr("type");
+				var id = $(e.currentTarget).attr("id");
+				this.$router.push({path: '/tradeDetails', query: {type: type, id: id}});
+			},
+			getOperateDetails: function(){
+				this.$http.post(this.PATH + '/user/ftrade/list', {emulateJSON: true}, {
+					headers: {
+						'token':  this.userInfo.token,
+						'secret': this.userInfo.secret
+					},
+					params: {},
+					timeout: 5000
+				}).then(function(e) {
+					var data = e.body;
+					if(data.success == true ){
+						if(data.code == 1){
+	            			this.tradeList = data.data.tradeList;
+	            			if(this.tradeList == null) this.emptyShow = true;
+						}
+					}else{
+						switch (data.code){
+							case '-1':
+								this.$refs.dialog.isShow = true;
+								this.msg = '认证失败';
+								break;
+							default:
+								break;
+						}
+					}
+				}.bind(this), function() {
+					this.$refs.dialog.isShow = true;
+					this.msg = '网络不给力，请稍后再试！'
+				});
 			}
 		},
 		mounted: function() {
+			//初始化页面高度
+			var h = window.screen.height - $("#topbar").height() - $("#navbar").height() - 20;
+			$(".main").height(h);
 			$('.margin_trade>ul>li:first-child>div>div').addClass('current');
 			//初始化banner
 			this.getBanner();
 			var gallery = mui('.mui-slider');
 			gallery.slider({
-			    interval: 0 //自动轮播周期，若为0则不自动播放，默认为0；
+			    interval: 0 //自动轮播周期，若为0则不自动播放，默认为0;
+			});
+			//保证金置顶
+			$(".main").scroll(function(){
+				var scroH = $(this).scrollTop();
+				var h = $(".margin_trade").offset().top;
+				if(scroH >= h){
+					$(".margin_trade").css({
+						"position": "fixed",
+						"top": 50,
+						"z-index": 1111
+					})
+				}else{
+					$(".margin_trade").css({
+						"position": "static",
+					});
+				}
 			});
 		},
 		activated: function() {
+			//初始化页面显示申请开户
+			this.isselected = 0;
+			this.show = true;
+			//获取本地用户信息
+			this.userInfo = JSON.parse(localStorage.user);
+			//获取操盘记录列表
+			this.getOperateDetails();
+			//判断是否登录
 			if(!localStorage.user) {
 				this.$router.replace({
 					'path': '/login'
@@ -553,11 +728,16 @@
 		#applydetail {
 			padding: 50px 0;
 		}
+		.main{
+			width: 100%;
+			overflow-y: auto;
+		}
 		.margin_trade {
-			height: 155px;
+			height: 158px;
 			overflow: hidden;
 			background: #242633;
 			padding: 0 15px;
+			border-bottom: 5px solid #1b1b26;
 			p {
 				margin-top: 15px;
 			}
@@ -572,9 +752,9 @@
 			}
 		}
 		.margin_trade_show {
+			overflow: hidden;
 			background: #242633;
 			border-bottom: 5px solid #1b1b26;
-			border-top: 5px solid #1b1b26;
 			li {
 				height: 40px;
 				line-height: 40px;
@@ -602,7 +782,7 @@
 			background: @deepblue;
 			overflow-x: scroll;
 			ul{
-				width: 880px;
+				width: 930px;
 				&.list_head li span{
 					&:first-child{
 						color: @blue;
@@ -636,7 +816,7 @@
 						width: 200px;
 					}
 					&:nth-child(4){
-						width: 150px;
+						width: 200px;
 					}
 					&:nth-child(5){
 						width: 250px;
@@ -726,7 +906,104 @@
 				}
 			}
 		}
-		
+		.page_cont{
+    		width: 100%;
+    		overflow-y: scroll;
+    		li{
+    			overflow: hidden;
+    			background: @deepblue;
+    			margin-bottom: 10px;
+    			&.settlement{
+    				background: #2d3040;
+    			}
+    			&:last-child{
+    				margin: 0;
+    			}
+    			.record_title{
+    				height: 44px;
+    				padding: 0 15px;
+    				border-bottom: 1px solid @black;
+    				span{
+    					line-height: 44px;
+    					color: #7a7f99;
+    					font-size: @fs14;
+    					&:first-child{
+    						display: inline-block;
+    						height: 22px;
+    						line-height: 22px;
+    						padding: 0 15px;
+    						overflow: hidden;
+    						font-size: @fs14;
+    						color: @black;
+    						border-radius: 4px;
+    						margin: 11px;
+    					}
+    					&.yellow{
+    						background: @yellow;
+    					}
+    					&.blue{
+    						background: @blue;
+    					}
+    					&.deepblue{
+    						color: @blue;
+    						background: @deepblueshallow;
+    					}
+    					&.grey{
+    						background: #ddd;
+    					}
+    				}
+    				.icon_arrow{
+    					width: 24px;
+    					height: 44px;
+    					background: url(../assets/img/arrow.png) no-repeat right 15px;
+    					background-size: 8px 14px;
+    				}
+    			}
+    			.record_cont{
+    				height: 88px;
+    				padding: 18px 15px 12px 15px;
+    				p{
+    					float: left;
+    					width: 50%;
+    					line-height: 28px;
+    					font-size: @fs16;
+    					color: @blue;
+    					&:nth-child(2), &:nth-child(4){
+    						text-align: right;
+    					}
+    					span{
+    						color: @white;
+    						&.spe{
+    							font-size: @fs14;
+    							color: #7a7f99;
+    						}
+    					}
+    				}
+    			}
+    		}
+    	}
+    	.empty {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			margin: -46px*@ip6 0 0 -207px*@ip6;
+			width: 414px*@ip6;
+			height: 92px*@ip6;
+			overflow: hidden;
+			text-align: center;
+			.icon_none {
+				display: inline-block;
+				width: 84px*@ip6;
+				height: 56px*@ip6;
+				overflow: hidden;
+				background: url(../assets/img/none.png) no-repeat center center;
+				background-size: 100% 100%;
+			}
+			p {
+				text-align: center;
+				margin-top: 10px*@ip6;
+			}
+		}
 		
 		
 	}
