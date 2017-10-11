37e3979a9eb8038689885c51b8bcd1cc69445242
liunianliunian <cbeisheng_work@foxmail.com>
2017-10-11 18:34:28 +0800

    11

--- professional-webapp/src/App.vue
+++ professional-webapp/src/App.vue
@@ -1,6 +1,38 @@
 <template>
 	<div id="app">
-		<router-view/>
+		<div id="container">
+			<div id="container_left">
+				<ul>
+					<li>
+						
+					</li>
+					<li>行情</li>
+					<li>交易</li>
+					<li>开户</li>
+					<li>直播</li>
+					<li>日历</li>
+					<li>下载</li>
+					<li>我的</li>
+				</ul>
+			</div>
+			<div id="container_top">
+				<ul>
+					<li>放大</li>
+					<li>在线客服</li>
+					<li>客服热线</li>
+					<li>登录</li>
+					<li>注册</li>
+				</ul>
+			</div>
+			<div id="container_buttom">
+				<span>国际原油<label></label></span>
+				<span>美黄金<label></label></span>
+				<span>恒指期货<label></label></span>
+			</div>
+			
+			<div id="container_center"></div>
+		</div>
+		
 	</div>
 </template>
 
@@ -11,5 +43,6 @@
 </script>
 
 <style scoped lang="scss">
+	@import "/assets/css/common.scss"
 	
 </style>
