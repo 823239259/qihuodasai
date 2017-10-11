dadfec4aa67249382a2842f8695b31a2583c7a37
wuxuanfeng <527140631@qq.com>
2017-10-11 13:38:18 +0800

    webapp

---
+++ professional-webapp/src/assets/js/common.js
@@ -0,0 +1,5 @@
+/*  
+ * @description: 公用JS 
+ * @author: name  
+ * @update: time (2017-04-13 18:32)  
+ */
---
+++ professional-webapp/src/components/test.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="test">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'test',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		},
+		
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+	$red: green;
+	h1{
+		color: $red;
+	}
+</style>
