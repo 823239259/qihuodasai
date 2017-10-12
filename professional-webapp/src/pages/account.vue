ebe9d5852a6c9e6fc6ab6cfcaf950707838f926b
chenbeisheng <864654012@qq.com>
2017-10-12 21:00:27 +0800

    222

--- professional-webapp/src/pages/account.vue
+++ professional-webapp/src/pages/account.vue
@@ -1,6 +1,16 @@
 <template>
 	<div id="account">
-		account
+			<ul>
+				<li>
+					<span>账户概括</span>
+				</li>
+				<li>
+					<span>开户明细</span>
+				</li>
+				<li>
+					<span>安全设置</span>
+				</li>
+			</ul>
 	</div>
 </template>
 
@@ -10,7 +20,21 @@
 	}
 </script>
 
-<style scoped lang="scss">
-@import url("../assets/css/common.scss");
-
+<style lang="scss" scoped type="text/css">
+@import "../assets/css/common.scss";
+	#account {
+		margin-left: 120px;
+		li {
+			text-align: center;
+			float : left;	
+			width : 120px;
+			height : 30px;
+			margin : 5px;
+			line-height : 30px;
+			background-color : $blue;
+			&:nth-child(1){
+				color: $yellow;
+			}
+		}
+	}
 </style>
