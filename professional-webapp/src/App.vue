1c9fadb730520df19b0df3293f3c13a018815d41
chenbeisheng <864654012@qq.com>
2017-10-31 20:14:23 +0800

    222

--- professional-webapp/src/App.vue
+++ professional-webapp/src/App.vue
@@ -223,7 +223,11 @@
 							this.$router.push({path: '/download'});
 							break;
 						case 7:
-							this.$router.push({path: '/account'});
+							if(JSON.parse(localStorage.user == '')){
+								this.$router.push({path:'/login'})
+							}else{
+								this.$router.push({path: '/account'});
+							}
 							break;
 						default:
 							break;
