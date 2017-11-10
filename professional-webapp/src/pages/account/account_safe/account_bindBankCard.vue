a280bdbd225403436dc406ab0e02da4016d4f008
chenbeisheng <864654012@qq.com>
2017-11-10 17:12:16 +0800

    222

--- professional-webapp/src/pages/account/account_safe/account_bindBankCard.vue
+++ professional-webapp/src/pages/account/account_safe/account_bindBankCard.vue
@@ -13,18 +13,22 @@
 		</div>
 		<div class="account_bindBankCard_center">
 			<ul>
-				<li>
-					<input type="radio" v-model="radiobutton" value="哈哈">哈哈
-					<span>招商银行</span><span>尾号8603</span><label>默认</label>
-					<select name="管理">
-						<option value="1">设为默认</option>
-						<option value="2">取消</option>
+				<li  v-for="(k,index) in bindBankList" class="bankList" v-on:click="choosBank" :class="{curr:current1 == index}">
+					<i class="ifont">&#xe698;</i>
+					<span>{{k.bankName}}</span>
+					<span>尾号{{k.card.substr(-4,4)}}</span>
+					<label  v-if="k.default == true">默认</label>
+					<label  v-else="k.default!=true"></label>
+					<select id="manage" @change="chooseChandle">
+						<option value="1">管理</option>
+						<option value="2" v-if="k.default == true">设为默认</option>
+						<option value="2" v-else="k.default！=true">取消默认</option>
+						<option value="3">编辑</option>
+						<option value="4">删除</option>
 					</select>
 				</li>
-				<li>
-					<input type="radio" v-model="radiobutton" value="嘿嘿"/>嘿嘿
-				</li>
 			</ul>
+			<button class="btn yellow" v-on:click="toAddBankCard">添加银行卡</button>
 		</div>
 		<div class="account_bindBankCard_btm">
 			<p>投资有分析，入市需谨慎</p>
@@ -38,58 +42,167 @@
 		name : "safe_bindBankCard",
 		data(){
 			return{
-				bankList : '',
-				radiobutton:"二百五"
+				bindBankList : [],
+				bankId:'',
+				current1:0
 			}
 		},
 		methods:{
+			choosBank:function(e){
+				var index = $(e.currentTarget).index();
+				return this.bankId = this.bindBankList[index].bankId;
+			},
+			//select事件
+			chooseChandle:function(e){
+				var index = $("#manage option:selected").val();
+				switch (index){
+					case "2":
+						this.setDeaultBank(this.bankId);
+						break;
+					case "3":
+						this.$router.push({path:'/safe_addBankCard'});
+						break;
+					case "4":
+						this.delBankCard(this.bankId);
+						break;
+					default:
+						break;
+				}
+			},
 			//添加银行卡
 			toAddBankCard:function(){
 				this.$router.push({path:'/safe_addBankCard'})
 			},
 			//删除银行卡
-//			delBankCard:function(){
-//				pro.fetch("post",'/user/withdraw/del_bank',data,headers).then((res)=>{
-//					if(res.success == true){
-//						if(res.code == 1){
-//							layer.msg('删除成功',{time:1000});
-//						}else if(res.code == 5){
-//							layer.msg("该银行卡正在提现处理中，不能删除",{time:1000})
-//						}
-//					}
-//				}).catch((err)=>{
-//					layer.msg("网络不给力，请稍后再试",{time:1000})
-//				})
-//			},
+			delBankCard:function(){
+				var headers = {
+					token : JSON.parse(localStorage.user).token,
+					secret : JSON.parse(localStorage.user).secret
+				}
+				layer.open({
+					title:"删除银行卡",
+				    type: 1,
+				    skin: 'layui-layer-demo', 
+				    anim: 2,
+				    shadeClose: true, 
+				    content: '删除银行卡将无法提现到该银行卡中，确认删除？',
+				    btn:['确认','取消'],
+				    btn1:function(){
+						pro.fetch("post",'/user/withdraw/del_bank',{bankId:delbankid},headers).then((res)=>{
+							if(res.success == true){
+								if(res.code == 1){
+									layer.msg('删除成功',{time:1000});
+									//重新拉取已绑定银行卡
+									this.bindBankList = [];
+									this.getBindBankList();
+								}
+							}
+						}).catch((err)=>{
+							if(err.data.success == false){
+								switch (err.data.code){
+									case '-1':
+										layer.msg("认证失败，参数错误或为空",{time:2000});
+										break;
+									case '2':
+										layer.msg("删除失败",{time:2000});
+										break;
+									case '3':
+										layer.msg("用户信息不存在",{time:2000});
+										break;
+									case '4':
+										layer.msg("银行卡信息不存在",{time:2000});
+										break;
+									case '5':
+										layer.msg("该银行卡正在提现中，不能删除",{time:2000});
+										break;
+									default:
+										break;
+								}
+							}
+							layer.msg("网络不给力，请稍后再试",{time:1000})
+						})
+					},
+					btn2:function(){
+						this.$router.push({path:"/safe_bindBankCard"});
+					}.bind(this)
+				});
+			},
 			//设置默认
-//			setDeaultBank:function(){
-//				pro.fetch("post",'/user/withdraw/set_default_bank',data,headers).then((res)=>{
-//					if(res.success == true){
-//						if(res.code == 1){
-//							layer.msg('设置默认成功',{time:1000})
-//						}else if(res.code == 3){
-//							layer.msg('银行卡不存在',{time:1000})
-//						}
-//					}
-//				}).catch((err)=>{
-//					layer.msg('网络不给力，请稍后再试',{time:1000})
-//				})
-//			}
+			setDeaultBank:function(setDefaultBank){
+				var headers = {
+					token : JSON.parse(localStorage.user).token,
+					secret : JSON.parse(localStorage.user).secret
+				}
+				layer.open({
+					title:"设为默认",
+				    type: 1,
+				    skin: 'layui-layer-demo', 
+				    anim: 2,
+				    shadeClose: true, 
+				    content: '确认将该银行卡设为默认提现银行卡？',
+				    btn:['确认','取消'],
+				    btn1:function(setDefaultBank){
+				    	pro.fetch("post",'/user/withdraw/set_default_bank',{bankId:setDefaultBank},headers).then((res)=>{
+							if(res.success == true){
+								if(res.code == 1){
+									layer.msg('设置默认成功',{time:1000})
+									//重新拉取选项卡
+									this.bindBankList = [];
+									this.getBindBankList();
+								}
+							}
+						}).catch((err)=>{
+							if(err.data.success == false){
+								switch (err.data.code){
+									case -1:
+									layer.msg("认证失败，参数错误或为空",{time:2000});
+										break;
+									case 3:
+									layer.msg("银行卡不存在",{time:2000});
+										break
+									default:
+										break;
+								}
+							}
+							layer.msg('网络不给力，请稍后再试',{time:1000})
+						})
+				    },
+				    btn2:function(){
+				    	this.$router.push({path:"/safe_bindBankCard"});
+				    }.bind(this)
+				})
+			},
+			//获取已绑定银行卡
+			getBindBankList:function(){
+				pro.fetch('post','/user/withdraw/ bank_list','',
+				{
+					token : JSON.parse(localStorage.user).token,
+					secret : JSON.parse(localStorage.user).secret
+				}).then((res)=>{
+					var data = res.data 
+					if(res.success == true){
+						if(res.code == 1){
+							data.forEach(function(o,i){
+								this.bindBankList.push(o);
+							}.bind(this));
+						}
+					}
+				}).catch((err)=>{
+					if(err.data.success == false){
+						layer.msg("获取用户信息失败，请重试",{time:2000});
+					}else{
+						layer.msg('网络不给力，请稍后再试',{time:1000});
+					}
+				})
+			}
 		},
 		mounted : function(){
-			//获取绑定银行卡
-			pro.fetch('post','/user/withdraw/ bank_list','',{
-				token : JSON.parse(localStorage.user).token,
-				secret : JSON.parse(localStorage.user).secret
-			}).then((res)=>{
-				if(res.success == true){
-					if(res.code == 1){
-						this.bankList = res.data;
-					}
-				}
-			}).catch((err)=>{
-				layer.msg('网络不给力，请稍后再试',{time:1000})
-			})
+			//获取已绑定的银行卡
+			this.bindBankList = [];
+			this.getBindBankList();
+		},
+		activated: function(){
+			
 		}
 	}
 </script>
@@ -128,14 +241,30 @@
 			li{
 				width: 400px;
 				height: 40px;
-				border: 1px solid $yellow;
 				display: inline-block;
 				margin: auto;
+				border-radius: 5px;
 				line-height: 40px;
+				margin-top: 30px;
+				span{
+					float: left;
+					margin-left: 10px;
+					&:nth-child(2){
+						color: white;
+						font-size: $fs16;
+					}
+				}
+				label{
+					float: left;
+					margin-left: 10px;
+					color: $white;
+				}
 			}
 			.btn {
 				width: 160px;
 				height: 30px;
+				margin-top: 20px;
+				color: black;
 			}
 			input{
 				width: 20px;
@@ -144,11 +273,12 @@
 				border-radius: 10px;
 			}
 			select {
-				width: 50px;
-				height: 20px;
-				background-color: $bottom_color;
-				color: $white;
-				
+				background-color: $blue;
+				color: $lightblue;
+				float: right;
+				border: none;
+				margin-right: 20px;
+				line-height: 38px;
 			}
 		}
 		.account_bindBankCard_btm {
@@ -161,5 +291,14 @@
 				line-height: 40px;
 			}
 		}
+	}
+	.ifont {
+		color: $yellow;
+		font-size: $fs16;
+		float: left;
+		margin-left: 20px;
+	}
+	.curr{
+		border: 1px solid $yellow;
 	}
 </style>
