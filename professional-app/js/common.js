3b34189f6a1243ad251a0d820dcc16b784d3bce1
chenbeisheng <864654012@qq.com>
2017-11-17 16:44:55 +0800

    222

--- professional-app/js/common.js
+++ professional-app/js/common.js
@@ -520,7 +520,7 @@ var tzdr = {
 	operation: function() {
 		document.getElementById("quickMode").addEventListener("tap", function() {
 			if(!mui.cacheUser.isLogin()){
-				mui.confirm("您还未登录平台，请您先登录","登录提示",["否","是"],function(e){
+				mui.confirm("您还未登录，请您先登录","登录提示",["否","是"],function(e){
 					if (e.index == 1) {
 		                        mui.openWindow({
 		                        	url:"../login/login.html",
