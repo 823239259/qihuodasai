3c4a2715ba6b0f0f49a8b7a29de222faff17ddb8
wuxuanfeng <527140631@qq.com>
2017-12-12 09:17:23 +0800

    111

--- professional-webapp/src/pages/trade.vue
+++ professional-webapp/src/pages/trade.vue
@@ -959,20 +959,26 @@
 						}
 					};
 				}else{
-					b = {
-						"Method": 'InsertOrder',
-						"Parameters":{
-							"ExchangeNo": this.currentdetail.ExchangeNo,
-							"CommodityNo": this.currentdetail.CommodityNo,
-							"ContractNo": this.currentdetail.LastQuotation.ContractNo,
-							"OrderNum": this.defaultNum,
-							"Drection": 0,
-							"PriceType": 0,
-							"LimitPrice": parseFloat(this.tradePrices),
-							"TriggerPrice": 0,
-							"OrderRef": this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++)
-						}
-					};
+					if(this.tradePrices == '' || this.tradePrices <= 0 || this.tradePrices == undefined){
+						layer.msg('请输入限价', {time: 1000});
+					}else if(this.defaultNum == 0){
+						layer.msg('请输入手数', {time: 1000});
+					}else{
+						b = {
+							"Method": 'InsertOrder',
+							"Parameters":{
+								"ExchangeNo": this.currentdetail.ExchangeNo,
+								"CommodityNo": this.currentdetail.CommodityNo,
+								"ContractNo": this.currentdetail.LastQuotation.ContractNo,
+								"OrderNum": this.defaultNum,
+								"Drection": 0,
+								"PriceType": 0,
+								"LimitPrice": parseFloat(this.tradePrices),
+								"TriggerPrice": 0,
+								"OrderRef": this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++)
+							}
+						};
+					}
 				}
 				//确定文案
 				var contract = b.Parameters.CommodityNo + b.Parameters.ContractNo;
@@ -1010,20 +1016,26 @@
 						}
 					};
 				}else{
-					b = {
-						"Method": 'InsertOrder',
-						"Parameters":{
-							"ExchangeNo": this.currentdetail.ExchangeNo,
-							"CommodityNo": this.currentdetail.CommodityNo,
-							"ContractNo": this.currentdetail.LastQuotation.ContractNo,
-							"OrderNum": this.defaultNum,
-							"Drection": 1,
-							"PriceType": 0,
-							"LimitPrice": parseFloat(this.tradePrices),
-							"TriggerPrice": 0,
-							"OrderRef": this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++)
-						}
-					};
+					if(this.tradePrices == '' || this.tradePrices <= 0 || this.tradePrices == undefined){
+						layer.msg('请输入限价', {time: 1000});
+					}else if(this.defaultNum == 0){
+						layer.msg('请输入手数', {time: 1000});
+					}else{
+						b = {
+							"Method": 'InsertOrder',
+							"Parameters":{
+								"ExchangeNo": this.currentdetail.ExchangeNo,
+								"CommodityNo": this.currentdetail.CommodityNo,
+								"ContractNo": this.currentdetail.LastQuotation.ContractNo,
+								"OrderNum": this.defaultNum,
+								"Drection": 1,
+								"PriceType": 0,
+								"LimitPrice": parseFloat(this.tradePrices),
+								"TriggerPrice": 0,
+								"OrderRef": this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++)
+							}
+						};
+					}
 				}
 				//确定文案
 				var contract = b.Parameters.CommodityNo + b.Parameters.ContractNo;
