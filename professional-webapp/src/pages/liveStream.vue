d82df034199cfec7296921c080184096dde33b6c
chenbeisheng <864654012@qq.com>
2017-12-15 17:58:57 +0800

    222

--- professional-webapp/src/pages/liveStream.vue
+++ professional-webapp/src/pages/liveStream.vue
@@ -6,7 +6,7 @@
 				<li>({{showNowDay}}&nbsp;{{showNowTime}})</li>
 			</ul>
 			<ul>
-				<li><i class="ifont ifont_click" v-on:click="autoRefresh">&#xe600;</i>{{time}}</li>
+				<li><i class="ifont ifont_click" v-on:click="autoRefresh">&#xe634;</i>{{time}}</li>
 				<li>秒后自动刷新</li>
 				<li><i class="ifont ifont_refresh" v-on:click="handRefresh">&#xe6d1;</i></li>
 			</ul>
@@ -54,7 +54,7 @@
 				showbeforeDay:false,
 				show_beforeList:false,
 				colorState:false,
-				time:60,
+				time:10,
 				showNowTime:'',
 				showNowDay:'',
 				timing:'',
@@ -92,22 +92,21 @@
 			},
 			//自动刷新
 			autoRefresh:function(e){
-				if(this.colorState == false){
-					this.colorState=true;
-					$(e.currentTarget).css("color","#ffd400");
+				var juage = $(e.currentTarget).hasClass("ifont_click1");
+				if(juage){
+					$(e.currentTarget).removeClass("ifont_click1").html("&#xe634;").css("color","#a3aacc");
+					this.time = 10;
+					clearInterval(this.timing);
+				}else{
+					$(e.currentTarget).addClass("ifont_click1").html("&#xe600;").css("color","#ffd400");
 					this.timing = setInterval(function(){
 						this.time--;
 						if(this.time <= 0){
-							this.time =60;
+							this.time =10;
 							//刷新数据
 							this.getInfoList();
 						}
 					}.bind(this),1000);
-				}else if(this.colorState == true){
-					this.colorState=false;
-					$(e.currentTarget).css("color","#a3aacc");
-					this.time = 60;
-					clearInterval(this.timing);
 				}
 			},
 			getInfoList:function(){
@@ -118,7 +117,6 @@
 					maxTime:this.getNowFormatDate1(),
 					keyword:''
 				}
-				console.log(data)
 				pro.fetch('post','/crawler/getCrawler',data,{}).then((res)=>{
 					if(res.success == true){
 						if(res.code == ''){
