9ecb848bd7be9c15e62ad6dc9a604787774ec338
chenbeisheng <cbeisheng_work@foxmail.com>
2018-06-14 16:15:17 +0800

    222

--- professional-domestic/src/components/tradeBottom.vue
+++ professional-domestic/src/components/tradeBottom.vue
@@ -230,7 +230,8 @@
 							"PriceType": 1,
 							"LimitPrice": 0.00,
 							"TriggerPrice": 0,
-							"OrderRef": this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++)
+							"OrderRef": this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++),
+							"OpenCloseType":1
 						}
 					};
 					this.buyText = b;
@@ -266,7 +267,8 @@
 							"PriceType":1,
 							"LimitPrice":0.00,
 							"TriggerPrice":0,
-							"OrderRef":this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++)
+							"OrderRef":this.$store.state.market.tradeConfig.client_source+ new Date().getTime()+(buildIndex++),
+							"OpenCloseType":2
 						}
 					};
 					this.buyText = b;
